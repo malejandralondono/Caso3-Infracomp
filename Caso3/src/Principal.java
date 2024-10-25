97bb6d2bf66a5b063a79bd46a5a82c5f7ab13601
MATEO_LOPEZ <m.lopez112@uniandes.edu.co>
2024-10-25 10:32:16 -0500

    Opcion 1 done

--- Caso3/src/Principal.java
+++ Caso3/src/Principal.java
@@ -1,3 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Principal {
-    
+    static boolean runtime= true;
+    public static void main(String[] args) throws Exception{
+        Servidor servidor = new Servidor();
+        while (runtime){
+            System.out.println("Bienvenido al menú principal del caso 2. Selecciona una de las siguientes opciones:");
+            System.out.println("1. Opción 1");
+            System.out.println("2. Opción 2");
+            System.out.println("4. Salir");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String resp = reader.readLine();
+            if (resp.equals("1")) {
+                servidor.genASMKeys();
+                System.out.println("Funciona!");
+            }
+        }
+    }
 }
--- Caso3/src/Servidor.java
+++ Caso3/src/Servidor.java
@@ -1,14 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.*;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
 import javax.crypto.*;
 public class Servidor {
     
     public static final int PUERTO = 3400;
 
-    public static void main(String[] args) throws IOException{
+    public void comenzar() throws IOException{
         
         ServerSocket ss = null;
         boolean continuar = true;
@@ -29,4 +38,67 @@ public class Servidor {
             
         }
     }
+
+    public void genASMKeys () throws NoSuchAlgorithmException{
+        KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+        gen.initialize(1024);
+        KeyPair keyPair = gen.generateKeyPair();
+        
+        PrivateKey privadaKey = keyPair.getPrivate();
+        PublicKey publicaKey = keyPair.getPublic();
+        String prvkeystr = Base64.getEncoder().encodeToString(privadaKey.getEncoded());
+        String pubkeystr = Base64.getEncoder().encodeToString(publicaKey.getEncoded());
+        try {
+            FileWriter archivoPRIV = new FileWriter("Caso3/llave_privada/llave_priv.txt");
+            archivoPRIV.write(prvkeystr);
+            archivoPRIV.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            FileWriter archivoPUB = new FileWriter("Caso3/llave_publica/llave_pub.txt");
+            archivoPUB.write(pubkeystr);
+            archivoPUB.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static PublicKey getPublicKey() throws FileNotFoundException {
+        FileReader fr = new FileReader("llave_publica/llave_pub.txt");
+        BufferedReader br = new BufferedReader(fr);
+        PublicKey pubKey = null;
+        try {
+            String publicK = br.readLine();
+            byte[] publicBytes = Base64.getDecoder().decode(publicK);
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            pubKey = keyFactory.generatePublic(keySpec);
+            br.close();
+        } catch (Exception ex) {
+        ex.printStackTrace();
+        }
+        return pubKey;
+    }
+
+    public static PrivateKey getPrivateKey() throws FileNotFoundException {
+        FileReader fr = new FileReader("llave_privada/llave_priv.txt");
+        BufferedReader br = new BufferedReader(fr);
+        PrivateKey prvKey = null;
+        try {
+            String privateK = br.readLine();
+            byte[] privateBytes = Base64.getDecoder().decode(privateK);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            prvKey = keyFactory.generatePrivate(keySpec);
+            br.close();
+        } catch (Exception ex) {
+        ex.printStackTrace();
+        }
+        return prvKey;
+    }
+    
+
+
 }
